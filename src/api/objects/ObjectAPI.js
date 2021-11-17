537d72daeacfbc01c9075f3d88abf894b9d83dc8
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2021-11-17 10:30:02 -0800

    Notebook Snapshotting to the default Notebook isn't working #4469

--- src/api/objects/ObjectAPI.js
+++ src/api/objects/ObjectAPI.js
@@ -47,6 +47,7 @@ function ObjectAPI(typeRegistry, openmct) {
 
     this.rootProvider = new RootObjectProvider(this.rootRegistry);
     this.cache = {};
+    this.isSavingInprogress = false;
     this.interceptorRegistry = new InterceptorRegistry();
 
     this.SYNCHRONIZED_OBJECT_TYPES = ['notebook', 'plan'];
@@ -329,6 +330,7 @@ ObjectAPI.prototype.isMissing = function (domainObject) {
  *          has been saved, or be rejected if it cannot be saved
  */
 ObjectAPI.prototype.save = function (domainObject) {
+    this.isSavingInprogress = true;
     let provider = this.getProvider(domainObject.identifier);
     let savedResolve;
     let savedReject;
@@ -363,6 +365,8 @@ ObjectAPI.prototype.save = function (domainObject) {
             result = provider.update(domainObject);
         }
     }
+
+    this.isSavingInprogress = false;
 
     return result;
 };
@@ -477,7 +481,7 @@ ObjectAPI.prototype.mutate = function (domainObject, path, value) {
 
     if (this.isTransactionActive()) {
         this.transaction.add(domainObject);
-    } else {
+    } else if (!this.isSavingInprogress) {
         this.save(domainObject);
     }
 };
